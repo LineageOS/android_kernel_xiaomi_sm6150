9034f647cacfff0fd48225771fd6f54f620f6340
Ethan Chen <intervigil@gmail.com>
2015-02-27 18:31:48 -0800

    power: reset: allow device to preserve memory on restart
    * Always perform a soft reset as this preserves memory contents,
      including pstore and other persistent memory.
    
    Change-Id: Ideca44c9f38d2ebcd437ff289f8e036922eafcd2

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -278,6 +278,7 @@ static void halt_spmi_pmic_arbiter(void)
 
 static void msm_restart_prepare(const char *cmd)
 {
+	bool need_warm_reset = false;
 #ifdef CONFIG_QCOM_DLOAD_MODE
 	/* Write download mode flags if we're panic'ing
 	 * Write download mode flags if restart_mode says so
@@ -287,8 +288,12 @@ static void msm_restart_prepare(const char *cmd)
 	set_dload_mode(false);
 #endif
 
+#ifdef CONFIG_QCOM_PRESERVE_MEM
+	need_warm_reset = true;
+#endif
+
 	/* Hard reset the PMIC unless memory contents must be maintained. */
-	if (in_panic)
+	if (need_warm_reset || in_panic)
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
 	else
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_HARD_RESET);
