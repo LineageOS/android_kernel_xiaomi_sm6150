7c09e56589152a3b80b65fcc07d910ac2c36f4f0
Jaegeuk Kim <jaegeuk@google.com>
2019-02-05 14:36:37 -0800

    ext4: fix wrong QC drop which causes build error
    Wrong drop by QC.
    
    Bug: 123934649
    Change-Id: Ib6615f0765d00f04d2f411fe50767734217fcbd9
    Fixes: 367c46b11cb1 ("Enable hardware based FBE on f2fs and adapt ext4 fs")
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>

--- fs/ext4/page-io.c
+++ fs/ext4/page-io.c
@@ -484,8 +484,8 @@ int ext4_bio_write_page(struct ext4_io_submit *io,
 			gfp_flags = GFP_NOWAIT | __GFP_NOWARN;
 
 	retry_encrypt:
-	if (!fscrypt_using_hardware_encryption(inode))
-		bounce_page = fscrypt_encrypt_pagecache_blocks(page, PAGE_SIZE, 0, gfp_flags);
+		if (!fscrypt_using_hardware_encryption(inode))
+			bounce_page = fscrypt_encrypt_pagecache_blocks(page, PAGE_SIZE, 0, gfp_flags);
 		if (IS_ERR(bounce_page)) {
 			ret = PTR_ERR(bounce_page);
 			if (ret == -ENOMEM &&
