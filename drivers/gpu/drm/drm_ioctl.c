fd33befeb04b88a1aaca57147ec146b8c0685357
PIPIPIG233666 <pig.priv@gmail.com>
2019-12-01 13:10:26 -0500

    drm: Import Xiaomi changes
    Change-Id: I6e86500090b3f5547b1882acc0f92ca24f02acaa

--- drivers/gpu/drm/drm_ioctl.c
+++ drivers/gpu/drm/drm_ioctl.c
@@ -510,10 +510,10 @@ int drm_ioctl_permit(u32 flags, struct drm_file *file_priv)
 		return -EACCES;
 
 	/* MASTER is only for master or control clients */
-	if (unlikely((flags & DRM_MASTER) && 
+	if (unlikely((flags & DRM_MASTER) &&
 		     !drm_is_current_master(file_priv) &&
 		     !drm_is_control_client(file_priv)))
-		return -EACCES;
+			return -EACCES;
 
 	/* Control clients must be explicitly allowed */
 	if (unlikely(!(flags & DRM_CONTROL_ALLOW) &&
