DevriesL/Coral_ImageBreaker

drm: Introduce new exposure dim layer driver
This driver provides exposure adjustment function by Qcom SDE dim
layer without change panel hardware brightness to avoid PWM flicker
on OLED devices. Thanks to OnePlus' opensource code for inspiring
me to use dim layer.

Use expo_calc_backlight to remap brightness with hardware and SDE,
checking Disable Hardware Overlays in developer options if you face
blocks with differents brightness issue.

Signed-off-by: DevriesL <therkduan@gmail.com>

drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -2124,6 +2124,12 @@ static void _sde_crtc_blend_setup_mixer(struct drm_crtc *crtc,
 		for (i = 0; i < cstate->num_dim_layers; i++)
 			_sde_crtc_setup_dim_layer_cfg(crtc, sde_crtc,
 					mixer, &cstate->dim_layer[i]);
+#ifdef CONFIG_DRM_SDE_EXPO
+		if (cstate->exposure_dim_layer) {
+			_sde_crtc_setup_dim_layer_cfg(crtc, sde_crtc,
+					mixer, cstate->exposure_dim_layer);
+		}
+#endif
 	}
 
 	_sde_crtc_program_lm_output_roi(crtc);
@@ -3069,6 +3075,38 @@ static void _sde_crtc_set_input_fence_timeout(struct sde_crtc_state *cstate)
 	cstate->input_fence_timeout_ns *= NSEC_PER_MSEC;
 }
 
+#ifdef CONFIG_DRM_SDE_EXPO
+static void sde_crtc_build_dim_layer_expo(struct sde_hw_dim_layer *dim_layer,
+		int hdisplay, int vdisplay, u32 alpha) {
+	dim_layer->flags = SDE_DRM_DIM_LAYER_INCLUSIVE;
+	dim_layer->stage = SDE_STAGE_MAX - SDE_STAGE_0 - 1;
+	dim_layer->rect.x = 0;
+	dim_layer->rect.y = 0;
+	dim_layer->rect.w = hdisplay;
+	dim_layer->rect.h = vdisplay;
+	dim_layer->color_fill = (struct sde_mdss_color) {0, 0, 0, alpha};
+}
+
+static void _sde_crtc_set_dim_layer_expo(struct sde_crtc_state *cstate,
+        struct drm_crtc_state *state, uint64_t val)
+{
+	struct drm_display_mode *mode = &state->adjusted_mode;
+
+	if (!val) {
+		cstate->exposure_dim_layer = NULL;
+		return;
+	}
+
+	if (cstate->num_dim_layers == SDE_MAX_DIM_LAYERS - 1) {
+		SDE_ERROR("failed to get available dim layer for exposure\n");
+		return;
+	}
+
+	cstate->exposure_dim_layer = &cstate->dim_layer[cstate->num_dim_layers];
+	sde_crtc_build_dim_layer_expo(cstate->exposure_dim_layer, mode->hdisplay, mode->vdisplay, val);
+}
+#endif
+
 /**
  * _sde_crtc_clear_dim_layers_v1 - clear all dim layer settings
  * @cstate:      Pointer to sde crtc state
@@ -3091,13 +3129,23 @@ static void _sde_crtc_clear_dim_layers_v1(struct sde_crtc_state *cstate)
  * @cstate:      Pointer to sde crtc state
  * @user_ptr:    User ptr for sde_drm_dim_layer_v1 struct
  */
+#ifdef CONFIG_DRM_SDE_EXPO
+static void _sde_crtc_set_dim_layer_v1(struct sde_crtc_state *cstate,
+		struct drm_crtc_state *state, void __user *usr_ptr)
+#else
 static void _sde_crtc_set_dim_layer_v1(struct sde_crtc_state *cstate,
 		void __user *usr_ptr)
+#endif
 {
 	struct sde_drm_dim_layer_v1 dim_layer_v1;
 	struct sde_drm_dim_layer_cfg *user_cfg;
 	struct sde_hw_dim_layer *dim_layer;
+#ifdef CONFIG_DRM_SDE_EXPO
+	struct drm_display_mode *mode;
+	u32 count, i, alpha;
+#else
 	u32 count, i;
+#endif
 
 	if (!cstate) {
 		SDE_ERROR("invalid cstate\n");
@@ -3118,7 +3166,11 @@ static void _sde_crtc_set_dim_layer_v1(struct sde_crtc_state *cstate,
 	}
 
 	count = dim_layer_v1.num_layers;
+#ifdef CONFIG_DRM_SDE_EXPO
+	if (count > SDE_MAX_DIM_LAYERS - 1) {
+#else
 	if (count > SDE_MAX_DIM_LAYERS) {
+#endif
 		SDE_ERROR("invalid number of dim_layers:%d", count);
 		return;
 	}
@@ -3153,6 +3205,14 @@ static void _sde_crtc_set_dim_layer_v1(struct sde_crtc_state *cstate,
 				dim_layer[i].color_fill.color_2,
 				dim_layer[i].color_fill.color_3);
 	}
+#ifdef CONFIG_DRM_SDE_EXPO
+	alpha = sde_crtc_get_property(cstate, CRTC_PROP_DIM_LAYER_EXPO);
+	if (alpha) {
+		mode = &state->adjusted_mode;
+		cstate->exposure_dim_layer = &dim_layer[count];
+		sde_crtc_build_dim_layer_expo(cstate->exposure_dim_layer, mode->hdisplay, mode->vdisplay, alpha);
+	}
+#endif
 }
 
 /**
@@ -5730,6 +5790,10 @@ static void sde_crtc_install_properties(struct drm_crtc *crtc,
 					0x0, 0, ~0, 0, CRTC_PROP_DEST_SCALER);
 		}
 	}
+#ifdef CONFIG_DRM_SDE_EXPO
+	msm_property_install_volatile_range(&sde_crtc->property_info,
+			"dim_layer_exposure", 0x0, 0, ~0, 0, CRTC_PROP_DIM_LAYER_EXPO);
+#endif
 
 	sde_kms_info_add_keyint(info, "has_src_split", catalog->has_src_split);
 	sde_kms_info_add_keyint(info, "has_hdr", catalog->has_hdr);
@@ -5878,8 +5942,13 @@ static int sde_crtc_atomic_set_property(struct drm_crtc *crtc,
 		_sde_crtc_set_input_fence_timeout(cstate);
 		break;
 	case CRTC_PROP_DIM_LAYER_V1:
+#ifdef CONFIG_DRM_SDE_EXPO
+		_sde_crtc_set_dim_layer_v1(cstate, state,
+					(void __user *)(uintptr_t)val);
+#else
 		_sde_crtc_set_dim_layer_v1(cstate,
 					(void __user *)(uintptr_t)val);
+#endif
 		break;
 	case CRTC_PROP_ROI_V1:
 		ret = _sde_crtc_set_roi_v1(state,
@@ -5925,6 +5994,11 @@ static int sde_crtc_atomic_set_property(struct drm_crtc *crtc,
 			goto exit;
 		}
 		break;
+#ifdef CONFIG_DRM_SDE_EXPO
+	case CRTC_PROP_DIM_LAYER_EXPO:
+		_sde_crtc_set_dim_layer_expo(cstate, state, val);
+		break;
+#endif
 	default:
 		/* nothing to do */
 		break;

LineageOS/android_kernel_xiaomi_sm6150

Change-Id: I559e0828af186b4aa1ae186451882ea4aca7deb0

drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -2179,6 +2179,12 @@ static void _sde_crtc_blend_setup_mixer(struct drm_crtc *crtc,
 					mixer, cstate->fingerprint_dim_layer);
 			SDE_ATRACE_END("dim layer blend setup mixer");
 		}
+#ifdef CONFIG_DRM_SDE_EXPO
+		if (cstate->exposure_dim_layer) {
+			_sde_crtc_setup_dim_layer_cfg(crtc, sde_crtc,
+					mixer, cstate->exposure_dim_layer);
+		}
+#endif
 	}
 
 	_sde_crtc_program_lm_output_roi(crtc);
@@ -3224,6 +3230,38 @@ static void _sde_crtc_set_input_fence_timeout(struct sde_crtc_state *cstate)
 	cstate->input_fence_timeout_ns *= NSEC_PER_MSEC;
 }
 
+#ifdef CONFIG_DRM_SDE_EXPO
+static void sde_crtc_build_dim_layer_expo(struct sde_hw_dim_layer *dim_layer,
+		int hdisplay, int vdisplay, u32 alpha) {
+	dim_layer->flags = SDE_DRM_DIM_LAYER_INCLUSIVE;
+	dim_layer->stage = SDE_STAGE_MAX - 1;
+	dim_layer->rect.x = 0;
+	dim_layer->rect.y = 0;
+	dim_layer->rect.w = hdisplay;
+	dim_layer->rect.h = vdisplay;
+	dim_layer->color_fill = (struct sde_mdss_color) {0, 0, 0, alpha};
+}
+
+static void _sde_crtc_set_dim_layer_expo(struct sde_crtc_state *cstate,
+        struct drm_crtc_state *state, uint64_t val)
+{
+	struct drm_display_mode *mode = &state->adjusted_mode;
+
+	if (!val) {
+		cstate->exposure_dim_layer = NULL;
+		return;
+	}
+
+	if (cstate->num_dim_layers == SDE_MAX_DIM_LAYERS - 1) {
+		SDE_ERROR("failed to get available dim layer for exposure\n");
+		return;
+	}
+
+	cstate->exposure_dim_layer = &cstate->dim_layer[cstate->num_dim_layers];
+	sde_crtc_build_dim_layer_expo(cstate->exposure_dim_layer, mode->hdisplay, mode->vdisplay, val);
+}
+#endif
+
 /**
  * _sde_crtc_clear_dim_layers_v1 - clear all dim layer settings
  * @cstate:      Pointer to sde crtc state
@@ -3246,13 +3284,23 @@ static void _sde_crtc_clear_dim_layers_v1(struct sde_crtc_state *cstate)
  * @cstate:      Pointer to sde crtc state
  * @user_ptr:    User ptr for sde_drm_dim_layer_v1 struct
  */
+#ifdef CONFIG_DRM_SDE_EXPO
+static void _sde_crtc_set_dim_layer_v1(struct drm_crtc *crtc, struct drm_crtc_state *state,
+               struct sde_crtc_state *cstate, void __user *usr_ptr)
+#else
 static void _sde_crtc_set_dim_layer_v1(struct drm_crtc *crtc,
 		struct sde_crtc_state *cstate, void __user *usr_ptr)
+#endif
 {
 	struct sde_drm_dim_layer_v1 dim_layer_v1;
 	struct sde_drm_dim_layer_cfg *user_cfg;
 	struct sde_hw_dim_layer *dim_layer;
+#ifdef CONFIG_DRM_SDE_EXPO
+	struct drm_display_mode *mode;
+	u32 count, i, alpha;
+#else
 	u32 count, i;
+#endif
 	struct sde_kms *kms;
 
 	if (!crtc || !cstate) {
@@ -3281,7 +3329,11 @@ static void _sde_crtc_set_dim_layer_v1(struct drm_crtc *crtc,
 	}
 
 	count = dim_layer_v1.num_layers;
+#ifdef CONFIG_DRM_SDE_EXPO
+	if (count > SDE_MAX_DIM_LAYERS - 1) {
+#else
 	if (count > SDE_MAX_DIM_LAYERS) {
+#endif
 		SDE_ERROR("invalid number of dim_layers:%d", count);
 		return;
 	}
@@ -3316,6 +3368,14 @@ static void _sde_crtc_set_dim_layer_v1(struct drm_crtc *crtc,
 				dim_layer[i].color_fill.color_2,
 				dim_layer[i].color_fill.color_3);
 	}
+#ifdef CONFIG_DRM_SDE_EXPO
+	alpha = sde_crtc_get_property(cstate, CRTC_PROP_DIM_LAYER_EXPO);
+	if (alpha) {
+		mode = &state->adjusted_mode;
+		cstate->exposure_dim_layer = &dim_layer[count];
+		sde_crtc_build_dim_layer_expo(cstate->exposure_dim_layer, mode->hdisplay, mode->vdisplay, alpha);
+	}
+#endif
 }
 
 ssize_t xm_fod_dim_layer_alpha_store(struct device *dev,
@@ -6101,6 +6161,10 @@ static void sde_crtc_install_properties(struct drm_crtc *crtc,
 					0x0, 0, ~0, 0, CRTC_PROP_DEST_SCALER);
 		}
 	}
+#ifdef CONFIG_DRM_SDE_EXPO
+	msm_property_install_volatile_range(&sde_crtc->property_info,
+			"dim_layer_exposure", 0x0, 0, ~0, 0, CRTC_PROP_DIM_LAYER_EXPO);
+#endif
 
 	sde_kms_info_add_keyint(info, "has_src_split", catalog->has_src_split);
 	sde_kms_info_add_keyint(info, "has_hdr", catalog->has_hdr);
@@ -6311,8 +6375,13 @@ static int sde_crtc_atomic_set_property(struct drm_crtc *crtc,
 		_sde_crtc_set_input_fence_timeout(cstate);
 		break;
 	case CRTC_PROP_DIM_LAYER_V1:
+#ifdef CONFIG_DRM_SDE_EXPO
+		_sde_crtc_set_dim_layer_v1(crtc, state, cstate,
+					(void __user *)(uintptr_t)val);
+#else
 		_sde_crtc_set_dim_layer_v1(crtc, cstate,
 					(void __user *)(uintptr_t)val);
+#endif
 		break;
 	case CRTC_PROP_ROI_V1:
 		ret = _sde_crtc_set_roi_v1(state,
@@ -6358,6 +6427,11 @@ static int sde_crtc_atomic_set_property(struct drm_crtc *crtc,
 			goto exit;
 		}
 		break;
+#ifdef CONFIG_DRM_SDE_EXPO
+	case CRTC_PROP_DIM_LAYER_EXPO:
+		_sde_crtc_set_dim_layer_expo(cstate, state, val);
+		break;
+#endif
 	default:
 		/* nothing to do */
 		break;