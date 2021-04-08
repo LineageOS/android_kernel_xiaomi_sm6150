244e6a69894fcfc0e5a202b67e0696a42faec7c1
Arian <arian.kulmer@web.de>
2021-04-09 00:24:42 +0200

    power: supply: maxim: Merge K6 unrelated changes from maxim_k6
    Change-Id: I9e528b1d4e23b707f47e06a095a7ec74515b5e90

--- drivers/power/supply/maxim/onewire_gpio.c
+++ drivers/power/supply/maxim/onewire_gpio.c
@@ -22,9 +22,9 @@
 #include <linux/device.h>
 #include <linux/spinlock.h>
 
-#define ow_info	pr_err
-#define ow_dbg	pr_err
-#define ow_err	pr_err
+#define ow_info	pr_info
+#define ow_dbg	pr_debug
+#define ow_err	pr_debug
 #define ow_log	pr_err
 
 #define DRV_STRENGTH_16MA		(0x7 << 6)
@@ -451,14 +451,14 @@ static int onewire_gpio_probe(struct platform_device *pdev)
 
 	onewire_data->ow_gpio_desc = gpio_to_desc(onewire_data->ow_gpio);
 	onewire_data->ow_gpio_chip = gpiod_to_chip(onewire_data->ow_gpio_desc);
-	
+
 	onewire_data->gpio_in_out_reg = devm_ioremap(&pdev->dev,
 					(uint32_t)onewire_data->onewire_gpio_level_addr, 0x4);
 	onewire_data->gpio_cfg66_reg = devm_ioremap(&pdev->dev,
 					(uint32_t)onewire_data->onewire_gpio_cfg_addr, 0x4);
 	ow_log("onewire_gpio_level_addr is %x; onewire_gpio_cfg_addr is %x", (uint32_t)(onewire_data->onewire_gpio_level_addr), (uint32_t)(onewire_data->onewire_gpio_cfg_addr));
 	ow_log("onewire_data->gpio_cfg66_reg is %x; onewire_data->gpio_in_out_reg is %x", (uint32_t)(onewire_data->gpio_cfg66_reg), (uint32_t)(onewire_data->gpio_in_out_reg));
-	
+
 	// create device node
 	onewire_data->dev = device_create(onewire_class,
 		pdev->dev.parent->parent, onewire_major, onewire_data, "onewirectrl");
