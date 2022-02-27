0e108564e3b23c694d6aabab3134821a59de6366
kerolosel3w <kerolos1710@gmail.com>
2022-02-27 13:54:34 +0200

    kernel: Boost for a short amount of time when zygote forks

--- kernel/fork.c
+++ kernel/fork.c
@@ -95,6 +95,7 @@
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
 #include <linux/simple_lmk.h>
+#include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
 
 #include <asm/pgtable.h>
@@ -2227,8 +2228,9 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
+	/* Boost to max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current))
+                cpu_input_boost_kick_max(50);
 		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
 
 	/*
