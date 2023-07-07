c15deef92dc3a8b764f6e0fcf7ce8e351b745bee
Arian <arian.kulmer@web.de>
2023-07-07 14:17:27 +0200

    proc: Add option to use HWC value as SKU and product SKU
    Change-Id: I20ff9d406b2384e66ae61171228da2025c62b53f

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,33 +3,77 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
-#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
-#include <asm/setup.h>
+
+#if defined (CONFIG_INITRAMFS_IGNORE_SKIP_FLAG) \
+	|| defined(CONFIG_CMDLINE_HWC_IS_SKU) \
+	|| defined(CONFIG_CMDLINE_HWC_IS_PRODUCT_SKU)
+#define ALTER_CMDLINE
 #endif
+
+#ifdef ALTER_CMDLINE
+#include <asm/setup.h>
 
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 #define INITRAMFS_STR_FIND "skip_initramf"
 #define INITRAMFS_STR_REPLACE "want_initramf"
 #define INITRAMFS_STR_LEN (sizeof(INITRAMFS_STR_FIND) - 1)
+#endif
+
+#if defined(CONFIG_CMDLINE_HWC_IS_SKU) || defined(CONFIG_CMDLINE_HWC_IS_PRODUCT_SKU)
+#define HWC_STR_FIND "androidboot.hwc="
+#define HWC_STR_LEN (sizeof(HWC_STR_FIND) - 1)
+#endif
+
+#ifdef CONFIG_CMDLINE_HWC_IS_SKU
+#define SKU_STR_INSERT " androidboot.hardware.sku="
+#endif
+
+#ifdef CONFIG_CMDLINE_HWC_IS_PRODUCT_SKU
+#define PRODUCT_SKU_STR_INSERT " androidboot.product.hardware.sku="
+#endif
 
 static char proc_command_line[COMMAND_LINE_SIZE];
 
 static void proc_command_line_init(void) {
-	char *offset_addr;
-
 	strcpy(proc_command_line, saved_command_line);
 
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+	char *offset_addr;
 	offset_addr = strstr(proc_command_line, INITRAMFS_STR_FIND);
-	if (!offset_addr)
-		return;
+	if (offset_addr)
+		memcpy(offset_addr, INITRAMFS_STR_REPLACE, INITRAMFS_STR_LEN);
+#endif
 
-	memcpy(offset_addr, INITRAMFS_STR_REPLACE, INITRAMFS_STR_LEN);
+#if defined(CONFIG_CMDLINE_HWC_IS_SKU) || defined(CONFIG_CMDLINE_HWC_IS_PRODUCT_SKU)
+    char* hwc_offset_addr;
+    char hwc_value[8] = "";
+
+    hwc_offset_addr = strstr(proc_command_line, HWC_STR_FIND);
+#endif
+
+#ifdef CONFIG_CMDLINE_HWC_IS_SKU
+    if (hwc_offset_addr) {
+        memcpy(hwc_value, hwc_offset_addr + HWC_STR_LEN,
+               strstr(hwc_offset_addr + HWC_STR_LEN, " ") - (hwc_offset_addr + HWC_STR_LEN));
+        strcat(proc_command_line, SKU_STR_INSERT);
+        strcat(proc_command_line, hwc_value);
+    }
+#endif
+
+#ifdef CONFIG_CMDLINE_HWC_IS_PRODUCT_SKU
+    if (hwc_offset_addr) {
+        memcpy(hwc_value, hwc_offset_addr + HWC_STR_LEN,
+               strstr(hwc_offset_addr + HWC_STR_LEN, " ") - (hwc_offset_addr + HWC_STR_LEN));
+        strcat(proc_command_line, PRODUCT_SKU_STR_INSERT);
+        strcat(proc_command_line, hwc_value);
+    }
+#endif
 }
 #endif
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+#ifdef ALTER_CMDLINE
 	seq_printf(m, "%s\n", proc_command_line);
 #else
 	seq_printf(m, "%s\n", saved_command_line);
@@ -51,7 +95,7 @@ static const struct file_operations cmdline_proc_fops = {
 
 static int __init proc_cmdline_init(void)
 {
-#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+#ifdef ALTER_CMDLINE
 	proc_command_line_init();
 #endif
 
