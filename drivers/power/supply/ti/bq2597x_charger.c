b5aa5448846663775ae83d11032409a9a6d4a2c7
Alexander Winkowski <dereference23@outlook.com>
2023-10-28 17:29:00 +0000

    power: supply: bq2597x_charger: Remove static symbols exports
    Change-Id: I5f5d74dbfd51dd0a4f55b3b244c6f2d78a45b404
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- drivers/power/supply/ti/bq2597x_charger.c
+++ drivers/power/supply/ti/bq2597x_charger.c
@@ -496,7 +496,6 @@ static int bq2597x_enable_charge(struct bq2597x *bq, bool enable)
 
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_enable_charge);
 
 static int bq2597x_check_charge_enabled(struct bq2597x *bq, bool *enabled)
 {
@@ -525,7 +524,6 @@ static int bq2597x_enable_wdt(struct bq2597x *bq, bool enable)
 				BQ2597X_WATCHDOG_DIS_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_enable_wdt);
 
 static int bq2597x_set_wdt(struct bq2597x *bq, int ms)
 {
@@ -549,7 +547,6 @@ static int bq2597x_set_wdt(struct bq2597x *bq, int ms)
 				BQ2597X_WATCHDOG_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_set_wdt);
 
 static int bq2597x_enable_batovp(struct bq2597x *bq, bool enable)
 {
@@ -567,7 +564,6 @@ static int bq2597x_enable_batovp(struct bq2597x *bq, bool enable)
 				BQ2597X_BAT_OVP_DIS_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_enable_batovp);
 
 static int bq2597x_set_batovp_th(struct bq2597x *bq, int threshold)
 {
@@ -590,7 +586,6 @@ static int bq2597x_set_batovp_th(struct bq2597x *bq, int threshold)
 				BQ2597X_BAT_OVP_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_set_batovp_th);
 
 static int bq2597x_enable_batovp_alarm(struct bq2597x *bq, bool enable)
 {
@@ -608,7 +603,6 @@ static int bq2597x_enable_batovp_alarm(struct bq2597x *bq, bool enable)
 				BQ2597X_BAT_OVP_ALM_DIS_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_enable_batovp_alarm);
 
 static int bq2597x_set_batovp_alarm_th(struct bq2597x *bq, int threshold)
 {
@@ -626,7 +620,6 @@ static int bq2597x_set_batovp_alarm_th(struct bq2597x *bq, int threshold)
 				BQ2597X_BAT_OVP_ALM_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_set_batovp_alarm_th);
 
 static int bq2597x_enable_batocp(struct bq2597x *bq, bool enable)
 {
@@ -644,7 +637,6 @@ static int bq2597x_enable_batocp(struct bq2597x *bq, bool enable)
 				BQ2597X_BAT_OCP_DIS_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_enable_batocp);
 
 static int bq2597x_set_batocp_th(struct bq2597x *bq, int threshold)
 {
@@ -662,7 +654,6 @@ static int bq2597x_set_batocp_th(struct bq2597x *bq, int threshold)
 				BQ2597X_BAT_OCP_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_set_batocp_th);
 
 static int bq2597x_enable_batocp_alarm(struct bq2597x *bq, bool enable)
 {
@@ -680,7 +671,6 @@ static int bq2597x_enable_batocp_alarm(struct bq2597x *bq, bool enable)
 				BQ2597X_BAT_OCP_ALM_DIS_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_enable_batocp_alarm);
 
 static int bq2597x_set_batocp_alarm_th(struct bq2597x *bq, int threshold)
 {
@@ -698,8 +688,6 @@ static int bq2597x_set_batocp_alarm_th(struct bq2597x *bq, int threshold)
 				BQ2597X_BAT_OCP_ALM_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_set_batocp_alarm_th);
-
 
 static int bq2597x_set_busovp_th(struct bq2597x *bq, int threshold)
 {
@@ -717,7 +705,6 @@ static int bq2597x_set_busovp_th(struct bq2597x *bq, int threshold)
 				BQ2597X_BUS_OVP_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_set_busovp_th);
 
 static int bq2597x_enable_busovp_alarm(struct bq2597x *bq, bool enable)
 {
@@ -735,7 +722,6 @@ static int bq2597x_enable_busovp_alarm(struct bq2597x *bq, bool enable)
 				BQ2597X_BUS_OVP_ALM_DIS_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_enable_busovp_alarm);
 
 static int bq2597x_set_busovp_alarm_th(struct bq2597x *bq, int threshold)
 {
@@ -753,7 +739,6 @@ static int bq2597x_set_busovp_alarm_th(struct bq2597x *bq, int threshold)
 				BQ2597X_BUS_OVP_ALM_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_set_busovp_alarm_th);
 
 static int bq2597x_enable_busocp(struct bq2597x *bq, bool enable)
 {
@@ -771,8 +756,6 @@ static int bq2597x_enable_busocp(struct bq2597x *bq, bool enable)
 				BQ2597X_BUS_OCP_DIS_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_enable_busocp);
-
 
 static int bq2597x_set_busocp_th(struct bq2597x *bq, int threshold)
 {
@@ -790,7 +773,6 @@ static int bq2597x_set_busocp_th(struct bq2597x *bq, int threshold)
 				BQ2597X_BUS_OCP_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_set_busocp_th);
 
 static int bq2597x_enable_busocp_alarm(struct bq2597x *bq, bool enable)
 {
@@ -808,7 +790,6 @@ static int bq2597x_enable_busocp_alarm(struct bq2597x *bq, bool enable)
 				BQ2597X_BUS_OCP_ALM_DIS_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_enable_busocp_alarm);
 
 static int bq2597x_set_busocp_alarm_th(struct bq2597x *bq, int threshold)
 {
@@ -826,7 +807,6 @@ static int bq2597x_set_busocp_alarm_th(struct bq2597x *bq, int threshold)
 				BQ2597X_BUS_OCP_ALM_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_set_busocp_alarm_th);
 
 static int bq2597x_enable_batucp_alarm(struct bq2597x *bq, bool enable)
 {
@@ -844,7 +824,6 @@ static int bq2597x_enable_batucp_alarm(struct bq2597x *bq, bool enable)
 				BQ2597X_BAT_UCP_ALM_DIS_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_enable_batucp_alarm);
 
 static int bq2597x_set_batucp_alarm_th(struct bq2597x *bq, int threshold)
 {
@@ -862,7 +841,6 @@ static int bq2597x_set_batucp_alarm_th(struct bq2597x *bq, int threshold)
 				BQ2597X_BAT_UCP_ALM_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_set_batucp_alarm_th);
 
 static int bq2597x_set_acovp_th(struct bq2597x *bq, int threshold)
 {
@@ -885,7 +863,6 @@ static int bq2597x_set_acovp_th(struct bq2597x *bq, int threshold)
 	return ret;
 
 }
-EXPORT_SYMBOL_GPL(bq2597x_set_acovp_th);
 
 static int bq2597x_set_vdrop_th(struct bq2597x *bq, int threshold)
 {
@@ -940,7 +917,6 @@ static int bq2597x_enable_bat_therm(struct bq2597x *bq, bool enable)
 				BQ2597X_TSBAT_DIS_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_enable_bat_therm);
 
 /*
  * the input threshold is the raw value that would write to register directly.
@@ -952,7 +928,6 @@ static int bq2597x_set_bat_therm_th(struct bq2597x *bq, u8 threshold)
 	ret = bq2597x_write_byte(bq, BQ2597X_REG_29, threshold);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_set_bat_therm_th);
 
 static int bq2597x_enable_bus_therm(struct bq2597x *bq, bool enable)
 {
@@ -970,7 +945,6 @@ static int bq2597x_enable_bus_therm(struct bq2597x *bq, bool enable)
 				BQ2597X_TSBUS_DIS_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_enable_bus_therm);
 
 /*
  * the input threshold is the raw value that would write to register directly.
@@ -982,8 +956,6 @@ static int bq2597x_set_bus_therm_th(struct bq2597x *bq, u8 threshold)
 	ret = bq2597x_write_byte(bq, BQ2597X_REG_28, threshold);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_set_bus_therm_th);
-
 
 static int bq2597x_enable_die_therm(struct bq2597x *bq, bool enable)
 {
@@ -1001,7 +973,6 @@ static int bq2597x_enable_die_therm(struct bq2597x *bq, bool enable)
 				BQ2597X_TDIE_DIS_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_enable_die_therm);
 
 /*
  * please be noted that the unit here is degC
@@ -1019,7 +990,6 @@ static int bq2597x_set_die_therm_th(struct bq2597x *bq, u8 threshold)
 				BQ2597X_TDIE_ALM_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_set_die_therm_th);
 
 static int bq2597x_enable_adc(struct bq2597x *bq, bool enable)
 {
@@ -1037,7 +1007,6 @@ static int bq2597x_enable_adc(struct bq2597x *bq, bool enable)
 				BQ2597X_ADC_EN_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_enable_adc);
 
 static int bq2597x_set_adc_average(struct bq2597x *bq, bool avg)
 {
@@ -1055,7 +1024,6 @@ static int bq2597x_set_adc_average(struct bq2597x *bq, bool avg)
 				BQ2597X_ADC_AVG_MASK, val);
 	return 0;
 }
-EXPORT_SYMBOL_GPL(bq2597x_set_adc_average);
 
 static int bq2597x_set_adc_scanrate(struct bq2597x *bq, bool oneshot)
 {
@@ -1073,7 +1041,6 @@ static int bq2597x_set_adc_scanrate(struct bq2597x *bq, bool oneshot)
 				BQ2597X_ADC_EN_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_set_adc_scanrate);
 
 static int bq2597x_set_adc_bits(struct bq2597x *bq, int bits)
 {
@@ -1092,7 +1059,6 @@ static int bq2597x_set_adc_bits(struct bq2597x *bq, int bits)
 				BQ2597X_ADC_SAMPLE_MASK, val);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_set_adc_bits);
 
 #define ADC_REG_BASE 0x16
 static int bq2597x_get_adc_data(struct bq2597x *bq, int channel,  int *result)
@@ -1123,7 +1089,6 @@ static int bq2597x_get_adc_data(struct bq2597x *bq, int channel,  int *result)
 
 	return 0;
 }
-EXPORT_SYMBOL_GPL(bq2597x_get_adc_data);
 
 static int sc8551_optimize_adc(struct bq2597x *bq)
 {
@@ -1182,7 +1147,6 @@ static int bq2597x_set_alarm_int_mask(struct bq2597x *bq, u8 mask)
 
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_set_alarm_int_mask);
 
 static int bq2597x_clear_alarm_int_mask(struct bq2597x *bq, u8 mask)
 {
@@ -1199,7 +1163,6 @@ static int bq2597x_clear_alarm_int_mask(struct bq2597x *bq, u8 mask)
 
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_clear_alarm_int_mask);
 
 static int bq2597x_set_fault_int_mask(struct bq2597x *bq, u8 mask)
 {
@@ -1216,7 +1179,6 @@ static int bq2597x_set_fault_int_mask(struct bq2597x *bq, u8 mask)
 
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_set_fault_int_mask);
 
 static int bq2597x_clear_fault_int_mask(struct bq2597x *bq, u8 mask)
 {
@@ -1233,7 +1195,6 @@ static int bq2597x_clear_fault_int_mask(struct bq2597x *bq, u8 mask)
 
 	return ret;
 }
-EXPORT_SYMBOL_GPL(bq2597x_clear_fault_int_mask);
 
 static int sc8551_set_bypass_mode_en(struct bq2597x *bq, int enable)
 {
@@ -1279,7 +1240,6 @@ static int sc8551_set_charge_mode(struct bq2597x *bq, int mode)
 
 	return ret;
 }
-EXPORT_SYMBOL_GPL(sc8551_set_charge_mode);
 
 static int sc8551_get_charge_mode(struct bq2597x *bq)
 {
@@ -1293,7 +1253,6 @@ static int sc8551_get_charge_mode(struct bq2597x *bq)
 
 	return (int)(val & SC8551_CHARGE_MODE_MASK);
 }
-EXPORT_SYMBOL_GPL(sc8551_get_charge_mode);
 
 static int bq2597x_set_sense_resistor(struct bq2597x *bq, int r_mohm)
 {
@@ -2258,8 +2217,6 @@ static void bq2597x_dump_reg(struct bq2597x *bq)
 		}
 	}
 }
-EXPORT_SYMBOL_GPL(bq2597x_dump_reg);
-
 
 static void bq2597x_check_alarm_status(struct bq2597x *bq)
 {
