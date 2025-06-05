9c26523367d4dc4a32885bd0c84e324550097da9
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2025-06-05 14:09:57 +0530

    dsi-staging, nt36xxx_spi: Use violet's gesture mode logic for surya too
    Change-Id: Idb084fe99a90ea7b05119557f0f8c96515069a9b

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -487,7 +487,7 @@ exit:
 	return rc;
 }
 
-#ifdef CONFIG_MACH_XIAOMI_VIOLET
+#if defined(CONFIG_MACH_XIAOMI_SURYA) || defined(CONFIG_MACH_XIAOMI_VIOLET)
 extern bool enable_gesture_mode;
 #endif
 static int dsi_panel_power_off(struct dsi_panel *panel)
@@ -498,7 +498,7 @@ static int dsi_panel_power_off(struct dsi_panel *panel)
 	if (gpio_is_valid(panel->reset_config.disp_en_gpio))
 		gpio_set_value(panel->reset_config.disp_en_gpio, 0);
 
-#ifdef CONFIG_MACH_XIAOMI_VIOLET
+#if defined(CONFIG_MACH_XIAOMI_SURYA) || defined(CONFIG_MACH_XIAOMI_VIOLET)
 	skip_reset_gpio = enable_gesture_mode;
 #elif defined CONFIG_TOUCHSCREEN_TDDI_DBCLK
 	skip_reset_gpio = panel->is_tddi_flag && is_tp_doubleclick_enable();
