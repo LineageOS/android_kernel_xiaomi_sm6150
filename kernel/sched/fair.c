ab3789dfb87294bd93e70c76a67e905861b3ae80
theshaenix <theshaenix@gmail.com>
2025-09-12 21:03:59 +0530

    sched/fair: bore: sched_burst_smoothness_short = 1

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -189,7 +189,7 @@ unsigned int sysctl_sched_cfs_bandwidth_slice		= 5000UL;
 
 #ifdef CONFIG_SCHED_BORE
 uint __read_mostly sched_burst_smoothness_long  = 1;
-uint __read_mostly sched_burst_smoothness_short = 0;
+uint __read_mostly sched_burst_smoothness_short = 1;
 uint __read_mostly sched_burst_fork_atavistic   = 2;
 uint __read_mostly sched_burst_penalty_offset   = 22;
 uint __read_mostly sched_burst_penalty_scale    = 1280;
