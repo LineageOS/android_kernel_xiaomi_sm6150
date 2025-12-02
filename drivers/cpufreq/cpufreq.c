790c2916811c2db9e8fb53ae548b0c121db3d749
xellDart <mundorap2010@gmail.com>
2025-12-01 18:47:00 -0600

    Revert "cpufreq: Simplify logic for enable powersave governor when battery saver is on"
    This reverts commit 22909ce85f9401e32bb6d19c1ad5291a836fb98a.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -768,7 +768,9 @@ static ssize_t show_cpuinfo_cur_freq(struct cpufreq_policy *policy,
  */
 static ssize_t show_scaling_governor(struct cpufreq_policy *policy, char *buf)
 {
-	if ((policy->policy == CPUFREQ_POLICY_POWERSAVE) || is_battery_saver_on())
+	if (is_battery_saver_on())
+		return sprintf(buf, "powersave\n");
+	else if (policy->policy == CPUFREQ_POLICY_POWERSAVE)
 		return sprintf(buf, "powersave\n");
 	else if (policy->policy == CPUFREQ_POLICY_PERFORMANCE)
 		return sprintf(buf, "performance\n");
