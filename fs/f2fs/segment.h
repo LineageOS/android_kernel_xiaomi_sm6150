f54ed2eb4b4b7b9a49634317ffde5e2b8cc0090e
xellDart <mundorap2010@gmail.com>
2025-12-05 21:32:14 -0600

    fs: Lower reclaim segment and threshold • Xiaomi's default segment reclamation (4096), along with that of other OEMs, is designed for MIUI, HyperOS's, and other $hit stocks roms.   heavy I/O patterns (background services, frequent writes).   For AOSP: 1. Lower Overhead: Smaller Garbage Collection (GC) batches                reduce CPU/storage load.             2. Better Responsiveness: Avoid long GC pauses during light                usage.             3. Battery Savings: Less aggressive reclaiming reduces                power spikes.

--- fs/f2fs/segment.h
+++ fs/f2fs/segment.h
@@ -12,8 +12,8 @@
 #define NULL_SEGNO			((unsigned int)(~0))
 #define NULL_SECNO			((unsigned int)(~0))
 
-#define DEF_RECLAIM_PREFREE_SEGMENTS	5	/* 5% over total segments */
-#define DEF_MAX_RECLAIM_PREFREE_SEGMENTS	4096	/* 8GB in maximum */
+#define DEF_RECLAIM_PREFREE_SEGMENTS	3	/* 3% over total segments */
+#define DEF_MAX_RECLAIM_PREFREE_SEGMENTS	1024	/* 8GB in maximum */
 
 #define F2FS_MIN_SEGMENTS	9 /* SB + 2 (CP + SIT + NAT) + SSA + MAIN */
 
