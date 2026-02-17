df4a842f1908e6a0b8e24ee82ab52d7dc6247d4a
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2026-02-17 23:08:55 +0530

    drivers: Import surya power supply and usb changes
    - Also import missing changes on surya dtsi(s)
    
    Change-Id: I0ab03980e9cde6d7b24c8f3b9345f615fbed7bd6
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -2486,7 +2486,11 @@ void composite_suspend(struct usb_gadget *gadget)
 	spin_unlock_irqrestore(&cdev->lock, flags);
 
 	usb_gadget_set_selfpowered(gadget);
+#ifdef CONFIG_MACH_XIAOMI_SURYA
+	usb_gadget_vbus_draw(gadget, 500);
+#else
 	usb_gadget_vbus_draw(gadget, 2);
+#endif
 }
 
 void composite_resume(struct usb_gadget *gadget)
